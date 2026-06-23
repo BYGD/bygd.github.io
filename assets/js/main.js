439318b02d982cf1391311a813d1a4b61401a990
BYGD <mail@Cangdog.com>
2026-06-23 21:57:14 +0800

    随机背景图: Bing壁纸随机选取 + Picsum fallback

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,106 +1,93 @@
-var iUp = (function () {
-	var t = 0,
-		d = 150,
-		clean = function () {
-			t = 0;
-		},
-		up = function (e) {
-			setTimeout(function () {
-				$(e).addClass("up")
-			}, t);
-			t += d;
-		},
-		down = function (e) {
-			$(e).removeClass("up");
-		},
-		toggle = function (e) {
-			setTimeout(function () {
-				$(e).toggleClass("up")
-			}, t);
-			t += d;
-		};
-	return {
-		clean: clean,
-		up: up,
-		down: down,
-		toggle: toggle
-	}
-})();
-
-$(document).ready(function () {
-
-	// 获取一言数据
-	fetch('https://v1.hitokoto.cn').then(function (res) {
-		return res.json();
-	}).then(function (e) {
-		$('#description').html(e.hitokoto + "<br/> -「<strong>" + e.from + "</strong>」")
-	}).catch(function (err) {
-		console.error(err);
-	})
-
+	var iUp = (function () {
+		var t = 0,
+			d = 150,
+			clean = function () {
+				t = 0;
+			},
+			up = function (e) {
+				setTimeout(function () {
+					$(e).addClass("up")
+				}, t);
+				t += d;
+			},
+			down = function (e) {
+				$(e).removeClass("up");
+			},
+			toggle = function (e) {
+				setTimeout(function () {
+					$(e).toggleClass("up")
+				}, t);
+				t += d;
+			};
+		return {
+			clean: clean,
+			up: up,
+			down: down,
+			toggle: toggle
+		}
+	})();
 	
-	// var url = 'https://query.yahooapis.com/v1/public/yql' + 
-    // '?q=' + encodeURIComponent('select * from json where url=@url') +
-    // '&url=' + encodeURIComponent('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8') +
-	// '&format=json&callback=?';
+	$(document).ready(function () {
+	
+		// 获取一言数据
+		fetch('https://v1.hitokoto.cn').then(function (res) {
+			return res.json();
+		}).then(function (e) {
+			$('#description').html(e.hitokoto + "<br/> -「<strong>" + e.from + "</strong>」")
+		}).catch(function (err) {
+			console.error(err);
+		})
+	
+		
+		/**
+		 * 随机背景图
+		 * 优先从 Bing 每日壁纸中随机选取一张
+		 * 失败时 fallback 到 Lorem Picsum 随机图
+		 */
+		var $panel = $('#panel');
+		var bingApi = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
 
-	/**
-	 * 获取Bing壁纸
-	 * 原先 YQL 已经无法提供服务了
-	 * 改用 JsonBird：https://bird.ioliu.cn/
-	 * 
-	 */
-	var url = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
-	var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
-	var index = sessionStorage.getItem("index");
-	var $panel = $('#panel');
-	if(imgUrls == null){
-		imgUrls = new Array();
-		index = 0;		
-		$.get(url,function (result) {
-			images = result.images;
-			for (let i = 0; i < images.length; i++) {
-				const item = images[i];
-				imgUrls.push(item.url);
-			}
-			var imgUrl = imgUrls[index];
-			var url = "https://www.bing.com"+imgUrl;
-			$panel.css("background", "url('"+url+"') center center no-repeat #666");
+		function setBackground(imgUrl) {
+			$panel.css("background", "url('" + imgUrl + "') center center no-repeat #666");
 			$panel.css("background-size", "cover");
-			sessionStorage.setItem("imgUrls",JSON.stringify(imgUrls));
-			sessionStorage.setItem("index",index);
+		}
+
+		function randomBing() {
+			$.get(bingApi, function (result) {
+				var images = result.images;
+				if (images && images.length > 0) {
+					var idx = Math.floor(Math.random() * images.length);
+					var url = "https://www.bing.com" + images[idx].url;
+					setBackground(url);
+				}
+			}).fail(function () {
+				// Fallback: Lorem Picsum 随机图片
+				var fallback = 'https://picsum.photos/1920/1080?random=' + Date.now();
+				setBackground(fallback);
 			});
-	}else{
-		if(index == 7)
-			index = 0;
-		else
-			index++;
-		var imgUrl = imgUrls[index];
-		var url = "https://www.bing.com"+imgUrl;
-		$panel.css("background", "url('"+url+"') center center no-repeat #666");
-		$panel.css("background-size", "cover");
-		sessionStorage.setItem("index",index);
-	}
+		}
+
+		randomBing();
+		
+		$(".iUp").each(function (i, e) {
+			iUp.up(e);
+		});
 	
-	$(".iUp").each(function (i, e) {
-		iUp.up(e);
+		$(".js-avatar")[0].onload = function () {
+			$(".js-avatar").addClass("show");
+		}
 	});
-
-	$(".js-avatar")[0].onload = function () {
-		$(".js-avatar").addClass("show");
-	}
-});
-
-$('.btn-mobile-menu__icon').click(function() {
-    if ($('.navigation-wrapper').css('display') == "block") {
-      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-      });
-      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
-
-    } else {
-      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
-    }
-    $('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn');
-});
+	
+	$('.btn-mobile-menu__icon').click(function() {
+	    if ($('.navigation-wrapper').css('display') == "block") {
+	      $('.navigation-wrapper').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+	        $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+	        $('.navigation-wrapper').off('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
+	      });
+	      $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+	
+	    } else {
+	      $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+	    }
+	    $('.btn-mobile-menu__icon').toggleClass('social iconfont icon-list social iconfont icon-angleup animated fadeIn');
+	});
